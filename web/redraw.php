fdc71bf9b7fa0c37ef71f356f724b434b2c4c8ad
patwonder <patwonder@163.com>
2013-07-03 16:43:43 +0800

    Add header info for redraw.php

--- web/redraw.php
+++ web/redraw.php
@@ -9,11 +9,19 @@
     <link rel="stylesheet" type="text/css" href="redraw.css" />
     
     <script>
-      var obj = JSON.parse(<?php echo json_encode($_REQUEST['json']); ?>);
+      var obj = JSON.parse(<?php echo json_encode($_POST['json']); ?>);
     </script>
     <script src="redraw.js"></script>
   </head>
   <body>
+    <pre><?php 
+      $headers = apache_request_headers();
+
+      foreach ($headers as $header => $value) {
+        echo "$header: $value \n";
+      }
+      echo file_get_contents('php://input');
+    ?></pre>
     <div id="boundingBox"></div>
     <div id="viewport"></div>
     <div id="dependency"></div>
